e860ce902e2af53ba8ae9c34b664248b8b100160
Manisha-karkam <113667017+Manisha-karkam@users.noreply.github.com>
2022-11-18 10:34:25 +0530

    Update first.js

--- __tests__/first.js
+++ __tests__/first.js
@@ -8,6 +8,16 @@ describe("checking the todo list",() => {
             dueDate:new Date().toLocaleDateString("en-CA"),
         });
     });
+     add({
+            title:"preparing for exams",
+            completed:false,
+            dueDate:new Date().toLocaleDateString("en-CA"),
+        });
+      add({
+            title:"drinking 3 liters of water a day",
+            completed:true,
+            dueDate:new Date().toLocaleDateString("en-CA"),
+        });
     let length = total.length;
     test("creating new todo",()=>{
         expect(total.length).toBe(1);
@@ -20,21 +30,21 @@ describe("checking the todo list",() => {
     test("checking retrieval of overdue items",()=>{
         expect(
             duelist.every((todo)=>{
-                return todo.dueDate < new Date().toLocaleDateString("en-CA")
+                return todo.dueDate < new Date().toLocaleDateString("en-CA").toBe(true);
             }));
     });
     let todayduelist = dueToday();
     test("checking retrieval of dueToday items",()=>{
         expect(
             todayduelist.every((todo)=>{
-                return todo.dueDate === new Date().toLocaleDateString("en-CA")
+                return todo.dueDate === new Date().toLocaleDateString("en-CA").toBe(true);
             }));
     });
     let laterduelist = dueLater();
     test("checking retrieval of duelater items",()=>{
         expect(
             laterduelist.every((todo)=>{
-                return todo.dueDate > new Date().toLocaleDateString("en-CA")
+                return todo.dueDate > new Date().toLocaleDateString("en-CA").toBe(true);
             }));
     });
 });
